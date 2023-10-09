f90c6bc9887ba9e685ab3f1b73e79f608e3bdc91
Mikael Souza <107264460+mikael-souza@users.noreply.github.com>
2023-10-09 18:45:36 -0300

    upload inc

---
+++ inc/head.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Cálculo do Backoffice CF</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" href="font/iconsmind-s/css/iconsminds.css" />
+    <link rel="stylesheet" href="font/simple-line-icons/css/simple-line-icons.css" />
+
+    <link rel="stylesheet" href="css/vendor/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/vendor/bootstrap.rtl.only.min.css" />
+    <link rel="stylesheet" href="css/vendor/bootstrap-float-label.min.css" />
+    <link rel="stylesheet" href="css/vendor/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="css/vendor/smart_wizard.min.css" />
+    <link rel="stylesheet" href="css/vendor/bootstrap-tagsinput.css" />
+    <link rel="stylesheet" href="css/main.css" />
+</head>
---
+++ inc/menu.php
@@ -0,0 +1,20 @@
+<div class="menu">
+    <div class="main-menu">
+        <div class="scroll">
+            <ul class="list-unstyled">
+                <li>
+                    <a href="index.php">
+                        <i class="iconsminds-digital-drawing"></i>
+                        <span>Sistema de Calculo do Backoffice</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="documentos-necessarios.php">
+                        <i class="iconsminds-library"></i>
+                        <span>Enquadramento e documentos necessários</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
